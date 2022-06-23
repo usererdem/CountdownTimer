3f60c13877acfd467d8464bdb451b702ec0ae654
Erdem Elvan <erdemelvan35@gmail.com>
2022-06-23 10:52:11 +0300

    time updated

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 // 24 - 6 = 18:00:00 so (-6 for opening hour) and (+2 hours GMT difference) 
 /* const countToDate = new Date('June 25, 2022 18:00:00').setUTCHours(new Date().getUTCHours() -5) */
-const countToDate = new Date('June 25, 2022 18:00:00 GMT+1').getTime()
+const countToDate = new Date('June 25, 2022 19:00:00 GMT+1').getTime()
 console.log(countToDate);
 let previousTimeBetweenDates
 setInterval(() => {
